34ca6a5c24cdfa9bc7c63f9691f308ce156cc60a
Mohit Kalshan <mohitkalshan14.mk@gmail.com>
2025-08-17 17:57:25 +0530

    implement dto

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Controller('tasks')
 export class TasksController {
@@ -18,10 +19,11 @@ export class TasksController {
   @Post()
   createTask(
     // @Body() Body, this is for the whole body as urlencoded format
-    @Body('title') title: string, // for particuler title value pass 'title' key in @Body()
-    @Body('description') description: string,
+    // @Body('title') title: string, // for particuler title value pass 'title' key in @Body()
+    // @Body('description') description: string,
+    @Body() createTaskDto: CreateTaskDto, // implement dto for params
   ) {
-    console.log('request=>', title, description);
-    return this.taskService.createTask(title, description);
+    console.log('request=>', createTaskDto);
+    return this.taskService.createTask(createTaskDto);
   }
 }
